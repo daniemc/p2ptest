aace67a569a61b68c6c698cb3f7eda5db5e67e25
Daniel MC <daniel.meneses27@gmail.com>
2018-05-10 00:33:48 -0500

    add jobs to validate transactions after created

---
+++ app/Jobs/TransactionInitialValidation.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+use App\PlaceToPay\TransactionService;
+
+class TransactionInitialValidation implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $transactionID;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($transactionID)
+    {
+        $this->transactionID = $transactionID;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $transaction = new TransactionService();
+        $transaction->transactionInfo($this->transactionID);
+    }
+}
